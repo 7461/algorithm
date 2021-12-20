0fa4a4fedf1d025b698c425a40da6f1ca4261c29
gufei <gufei@antaiib.com>
2021-12-20 17:50:06 +0800

    排序算法

--- base/src/main/java/sort/SelectionSort.java
+++ base/src/main/java/sort/SelectionSort.java
@@ -6,17 +6,20 @@ package sort;
  * @createTime:2021/12/20 15:02
  */
 public class SelectionSort<T> implements Sort<T> {
-    public void sort(T[] arr,Comparator<T> comparator) {
-        if (arr==null|| arr.length<2){
+    public void sort(T[] arr, Comparator<T> comparator) {
+        if (arr == null || arr.length < 2) {
             return;
         }
         int length = arr.length;
-        for (int i=0;i<length-1;i++){
-            int nowIndex=i;
-            for (int j=i+1;j<length;j++){
-                if (comparator.compareTo(arr[i],arr[j]))
-                SwapUtil.swap(arr,nowIndex,j);
+        for (int i = 0; i < length; i++) {
+            int nowIndex = i;
+            int minIndex=nowIndex;
+            for (int j = i + 1; j < length; j++) {
+                if (comparator.compareTo(arr[minIndex], arr[j])) {
+                     minIndex = j;
+                }
             }
+            SwapUtil.swap(arr, nowIndex, minIndex);
         }
     }
 }
--- base/src/main/java/sort/Test.java
+++ base/src/main/java/sort/Test.java
@@ -11,9 +11,9 @@ public class Test {
     public static void main(String[] args) {
         Integer[] preIntegers = {9, 8, 6, 4, 11, 100, 0, 2, 2, 5};
         System.out.println(Arrays.asList(preIntegers));
-        //SortUtil.sort(preIntegers,new SelectionSort<Integer>(),new IntegerComparator());
+        SortUtil.sort(preIntegers,new SelectionSort<Integer>(),new IntegerComparator());
         //SortUtil.sort(preIntegers,new BubbleSort<Integer>(),new IntegerComparator());
-        SortUtil.sort(preIntegers,new InsertionSort<Integer>(),new IntegerComparator());
+        //SortUtil.sort(preIntegers,new InsertionSort<Integer>(),new IntegerComparator());
         System.out.println(Arrays.asList(preIntegers));
     }
 }
