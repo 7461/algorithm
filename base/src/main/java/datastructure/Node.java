043b4e37bd2eafba1d330927f96e91c4281e4aba
7461 <1294265815@qq.com>
2021-12-24 18:05:50 +0800

    单链表

---
+++ base/src/main/java/datastructure/Node.java
@@ -0,0 +1,16 @@
+package datastructure;
+
+import lombok.Data;
+
+/**
+ * @ClassName: Node
+ * @Author: guf
+ * @Date: 2021/12/24 17:50
+ * @Description:
+ */
+@Data
+public class Node {
+    public int value;
+    public Node next;
+
+}
--- base/src/main/java/datastructure/Test.java
+++ base/src/main/java/datastructure/Test.java
@@ -7,4 +7,29 @@ package datastructure;
  * @Description:数据结构
  */
 public class Test {
+    public static void main(String[] args) {
+        Node node1 = new Node();
+        Node node2 = new Node();
+        node1.setNext(node2);
+        Node node3 = new Node();
+        node2.setNext(node3);
+        Node node4 = new Node();
+        node2.setNext(node4);
+        Node node5 = new Node();
+        node2.setNext(node5);
+        Node reverse = reverse(node1);
+        System.out.println(reverse.value);
+    }
+
+    public static Node reverse(Node head) {
+        Node pre;
+        Node next;
+        while (head.next != null) {
+            next = head.next;
+            pre = head;
+            head = next;
+
+        }
+        return head;
+    }
 }
